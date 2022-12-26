fed74edf6ec4212da75dcd1001e720879bd3a967
ZhdanovskiyE <58247478+ZhdanovskiyE@users.noreply.github.com>
2022-12-26 15:16:32 +0500

    Добавьте файлы проекта.

---
+++ TwoSum/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace Prog_TwoSum
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] mass = { 1, 2, 3, 4, 5, 6, 7, 8 };
+            int target = 8;
+            for (int i = 0; i < mass.Length; i++)
+            { for (int j = i+1; j < mass.Length; j++)
+                {
+                    if (mass[i] + mass[j] == target)
+                        Console.WriteLine(mass[i]+ " ," + mass[j]);
+                }    
+            
+            }
+            Console.ReadLine();
+        }
+    }
+
+}
